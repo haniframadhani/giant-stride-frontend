3c05771ccacb582eda6df89cc393cb8ddd68679c
hanif ramadhani <mhaniframadhani985@gmail.com>
2023-04-09 14:53:25 +0700

    buat footer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
+import Footer from '@/components/components/footer'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />)
+  }
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
