bcab9d2b32a4513d1e9dcf0d6538806230e071a7
hanif ramadhani <mhaniframadhani985@gmail.com>
2023-04-09 11:31:27 +0700

    ubah font
    ubah font dari poppins jadi inter

--- src/components/components/button/index.js
+++ src/components/components/button/index.js
@@ -1,6 +1,6 @@
 export default function Button({ text }) {
   return (
-    <div className="capitalize w-fit rounded-lg cursor-pointer py-4 px-8 bg-orange-400 hover:bg-orange-500 active:bg-orange-600">
+    <div className="capitalize w-fit rounded-lg cursor-pointer py-4 px-8 bg-orange-400 hover:bg-orange-500 active:bg-orange-600 font-['Inter']">
       {text}
     </div>
   )
