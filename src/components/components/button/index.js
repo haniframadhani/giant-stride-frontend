831f0ec2e4ba9df1bfdf4799d7b8e179f3da1706
hanif ramadhani <mhaniframadhani985@gmail.com>
2023-04-11 11:12:27 +0700

    button dengan icon
    menambahkan icon pada button secara kondisional

--- src/components/components/button/index.js
+++ src/components/components/button/index.js
@@ -1,7 +1,10 @@
-export default function Button({ text }) {
+import { PlusIcon } from "@heroicons/react/24/outline";
+
+export default function Button({ text, iconPlus }) {
   return (
-    <div className="capitalize w-fit rounded-lg cursor-pointer py-4 px-8 bg-orange-400 hover:bg-orange-500 active:bg-orange-600 font-['Inter']">
+    <div className="capitalize w-fit rounded-lg cursor-pointer py-4 px-8 bg-orange-400 hover:bg-orange-500 active:bg-orange-600 font-['Inter'] text-white text-base flex gap-2">
       {text}
+      {iconPlus ? <PlusIcon className="w-6 h-6" /> : ''}
     </div>
   )
 }
