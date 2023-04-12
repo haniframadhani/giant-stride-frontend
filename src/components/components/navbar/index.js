685821f0e728b27415f9cafc3628bc0f5f2f5beb
hanif ramadhani <mhaniframadhani985@gmail.com>
2023-04-12 10:25:17 +0700

    ganti anchor jadi link

--- src/components/components/navbar/index.js
+++ src/components/components/navbar/index.js
@@ -1,5 +1,6 @@
 import { Bars2Icon, XMarkIcon } from "@heroicons/react/24/outline"
 import { useState } from "react"
+import Link from "next/link"
 
 export default function Navbar() {
   let links = [
@@ -11,13 +12,13 @@ export default function Navbar() {
   return (
     <header className="w-full sticky top-0 z-50">
       <div className="md:flex items-center justify-between bg-cyan-blue text-white py-4 md:px-10 px-7">
-        <a href="/"><h1 className="cursor-pointer uppercase font-bold text-2xl text-center">giant<br />spot</h1></a>
+        <Link href="/"><h1 className="cursor-pointer uppercase font-bold text-2xl text-center">giant<br />spot</h1></Link>
         <div onClick={() => setOpen(!open)} className="absolute right-8 top-6 cursor-pointer md:hidden">
           {!open ? <Bars2Icon className="w-12 h-12" /> : <XMarkIcon className="w-12 h-12" />}
         </div>
         <ul className={`md:flex md:items-center pb-12 md:pb-0 absolute bg-cyan-blue md:static md:z-auto z-[-1] left-0 w-full md:w-auto pl-9 md:pl-0 transition-all duration-500 ease-in ${open ? 'top-20' : 'top-[-490px]'} `}>
           {
-            links.map(link => <li key={link?.name} className="md:ml-8 text-xl my-7 md:my-0"><a href={link?.link} className="capitalize hover:underline duration-100">{link?.name}</a></li>)
+            links.map(link => <li key={link?.name} className="md:ml-8 text-xl my-7 md:my-0"><Link href={link?.link} className="capitalize hover:underline duration-100">{link?.name}</Link></li>)
           }
         </ul>
       </div>
