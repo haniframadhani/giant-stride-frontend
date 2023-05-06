b7ce68b7c36fb20545493cf13e3aebbfb3624b43
hanif ramadhani <mhaniframadhani985@gmail.com>
2023-05-06 13:24:29 +0700

    menghapus console.log

--- src/pages/blog/[...detail].js
+++ src/pages/blog/[...detail].js
@@ -2,7 +2,6 @@ export { default } from "@/components/pages/detail/index.js"
 
 export async function getServerSideProps(context) {
   const id = context.query.detail;
-  console.log('id : ', id)
   const responseBlogs = await fetch(`${process.env.REACT_APP_BASE_URL}api/article?id=${id}`);
   const blog = await responseBlogs.json();
   return {
