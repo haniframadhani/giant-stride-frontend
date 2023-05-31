9dd0636f440e8f69ae71c7cc20e226cac710849c
hanif ramadhani <mhaniframadhani985@gmail.com>
2023-05-31 09:42:30 +0700

    tambah flash message
    tambah flash message untuk pemberitahuan update berhasil atau tidak

--- src/components/components/flashMessage/index.js
+++ src/components/components/flashMessage/index.js
@@ -5,6 +5,7 @@ import { useContext, useEffect, useRef } from "react";
 import DeleteMessage from "./body/delete";
 import PostMessage from "./body/post";
 import { useRouter } from "next/router";
+import UpdateMessage from "./body/update";
 
 export default function Flash({ handleGetAllArticle, message }) {
   const flashRef = useRef(null);
@@ -16,8 +17,10 @@ export default function Flash({ handleGetAllArticle, message }) {
     setShowFlash(false)
     if (message === 'delete') {
       handleGetAllArticle();
-    } else if (message === 'post') {
-      push('/dashboard');
+    } else if (message === 'post' || message === 'update') {
+      if (success) {
+        push('/dashboard');
+      }
     }
   });
 
@@ -26,6 +29,10 @@ export default function Flash({ handleGetAllArticle, message }) {
       setShowFlash(false);
       if (message === 'delete') {
         handleGetAllArticle();
+      } else if (message === 'post' || message === 'update') {
+        if (success) {
+          push('/dashboard');
+        }
       }
     }, 3000);
   }, [])
@@ -33,7 +40,7 @@ export default function Flash({ handleGetAllArticle, message }) {
   return (
     <div className="bg-overlay">
       <div ref={flashRef} className="bg-white flex items-center gap-2 rounded-sm px-8 py-2 capitalize">
-        {message === 'delete' ? <DeleteMessage success={success} /> : <PostMessage success={success} />}
+        {message === 'delete' ? <DeleteMessage success={success} /> : message === 'post' ? <PostMessage success={success} /> : <UpdateMessage success={success} />}
       </div>
     </div>
   )
--- src/components/pages/update/index.js
+++ src/components/pages/update/index.js
@@ -1,8 +1,10 @@
 import Button from "@/components/components/button"
+import Flash from "@/components/components/flashMessage"
 import InputText from "@/components/components/inputs/text"
 import TextEditor from "@/components/components/texteditor"
 import { getOneArticle, updateArticle } from "@/components/utils/apiRequest"
 import { ChevronLeftIcon } from "@heroicons/react/24/outline"
+import openFlashMessagecontext from "@/components/contexts/openFlashMessagecontext"
 
 import Head from "next/head"
 import Link from "next/link"
@@ -13,6 +15,8 @@ export default function Update() {
   const router = useRouter();
   const id = router.query.articleId;
   const [post, setPost] = useState({});
+  const [showFlash, setShowFlash] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   const [updatePost, setUpdatePost] = useState({
     title: "",
@@ -38,10 +42,17 @@ export default function Update() {
           }
           return response.json()
         })
-        .then(result => { console.log(result) })
+        .then(result => {
+          if (result.status == 200) {
+            setSuccess(true);
+          } else {
+            setSuccess(false);
+          }
+        })
     } catch (err) {
-      console.log(err);
+      setSuccess(false);
     }
+    setShowFlash(true);
   }
 
   useEffect(() => {
@@ -61,19 +72,22 @@ export default function Update() {
 
   return (
     <div className="mx-8 md:mx-10 pt-20 pb-16 font-['Poppins']">
-      <Head>
-        <title>Update Article | Giant Spot</title>
-      </Head>
-      <Link href="/dashboard" className="flex items-center gap-2 w-fit">
-        <ChevronLeftIcon className="w-6 h-6" />
-        <h2 className="font-medium capitalize text-3xl">back</h2>
-      </Link>
-      <div className="mt-10 grid gap-4">
-        <img className="rounded w-1/3" src={post?.image} />
-        <InputText placeholder="Title e.g. my blog title" name="title" id="title" setTitle={setTitle} titleContent={post?.title} />
-        <TextEditor setBody={setBody} bodyContent={post?.body} />
-        <Button text='update' clickEvent={postArticle} />
-      </div>
+      <openFlashMessagecontext.Provider value={{ setShowFlash, success }}>
+        <Head>
+          <title>Update {post?.title} | Giant Spot</title>
+        </Head>
+        <Link href="/dashboard" className="flex items-center gap-2 w-fit">
+          <ChevronLeftIcon className="w-6 h-6" />
+          <h2 className="font-medium capitalize text-3xl">back</h2>
+        </Link>
+        <div className="mt-10 grid gap-4">
+          <img className="rounded w-1/3" src={post?.image} />
+          <InputText placeholder="Title e.g. my blog title" name="title" id="title" setTitle={setTitle} titleContent={post?.title} />
+          <TextEditor setBody={setBody} bodyContent={post?.body} />
+          <Button text='update' clickEvent={postArticle} />
+        </div>
+        {showFlash && (<Flash message='update' />)}
+      </openFlashMessagecontext.Provider>
     </div>
   )
 }
