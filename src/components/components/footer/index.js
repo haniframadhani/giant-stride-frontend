00b4d376acb389d924114f666532a5c1e9666070
hanif ramadhani <mhaniframadhani985@gmail.com>
2023-08-09 09:38:19 +0700

    feat(content): perbarui content dan perubahan minor styling
    merubah isi kontent dari halaman about dan perubahan warna selection

--- src/components/components/footer/index.js
+++ src/components/components/footer/index.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="py-6 mx-auto text-center text-light-gray font-light text-sm">
-      <p>copyright&#169; 2023 - GIANT SPOT</p>
+      <p>copyright&#169; 2023 - GIANT STRIDE</p>
     </footer>
   )
 }
