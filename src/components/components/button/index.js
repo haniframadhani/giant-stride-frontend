7e6bc46e315c7bdb970129bca1ee0a5937374bef
hanif ramadhani <mhaniframadhani985@gmail.com>
2023-08-15 11:45:18 +0700

    chore(login): menambahkan pesan error login
    menambahkan pesan error ketika error gagal

--- src/components/components/button/index.js
+++ src/components/components/button/index.js
@@ -1,23 +1,36 @@
-import { Login } from "@/components/utils/apiRequest";
 import { PlusIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/router";
 import { signIn } from "next-auth/react";
+import { useContext } from "react";
+import loginErrorContext from "@/components/contexts/loginErrorContext";
 
 export default function Button({ text, iconPlus, clickEvent, account }) {
-  const router = useRouter();
-  const login = async e => {
-    e.preventDefault();
-    const res = await signIn('credentials', {
-      email: account.email,
-      password: account.password,
-      redirect: false
-    })
-    if (res.status === 200) {
-      router.push('/dashboard');
+  if (account) {
+    const router = useRouter();
+    const { setErrorLogin } = useContext(loginErrorContext);
+    const login = async e => {
+      e.preventDefault();
+      const res = await signIn('credentials', {
+        email: account.email,
+        password: account.password,
+        redirect: false
+      })
+      if (res.status === 401) {
+        setErrorLogin(res)
+      }
+      if (res.status === 200) {
+        router.push('/dashboard');
+      }
     }
+    return (
+      <button onClick={account ? login : clickEvent} className="capitalize w-fit rounded-lg cursor-pointer py-4 px-8 bg-orange-400 hover:bg-orange-500 active:bg-orange-600 font-['Inter'] text-white text-base flex gap-2">
+        {text}
+        {iconPlus ? <PlusIcon className="w-6 h-6" /> : ''}
+      </button>
+    )
   }
   return (
-    <button onClick={account ? login : clickEvent} className="capitalize w-fit rounded-lg cursor-pointer py-4 px-8 bg-orange-400 hover:bg-orange-500 active:bg-orange-600 font-['Inter'] text-white text-base flex gap-2">
+    <button onClick={clickEvent} className="capitalize w-fit rounded-lg cursor-pointer py-4 px-8 bg-orange-400 hover:bg-orange-500 active:bg-orange-600 font-['Inter'] text-white text-base flex gap-2">
       {text}
       {iconPlus ? <PlusIcon className="w-6 h-6" /> : ''}
     </button>
