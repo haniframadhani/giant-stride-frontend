da2b6e9cf2770a623821f0a70b88401a5cac87f1
hanif ramadhani <mhaniframadhani985@gmail.com>
2023-08-20 09:51:31 +0700

    feat(config): tambah environment variable
    menambah environment variable di next.config.js untuk secret nextauth

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    REACT_APP_BASE_URL: process.env.REACT_APP_BASE_URL
+    REACT_APP_BASE_URL: process.env.REACT_APP_BASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
   }
 }
 
